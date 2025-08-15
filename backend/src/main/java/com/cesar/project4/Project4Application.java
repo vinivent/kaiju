1eb70a75d8761c90a4649aae39f22ffa74ebfff2
Vinícius Ventura <vini.ventura999@gmail.com>
2025-08-15 12:17:01 -0300

    first commit

---
+++ backend/src/main/java/com/cesar/project4/Project4Application.java
@@ -0,0 +1,14 @@
+package com.cesar.project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4Application.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/com/cesar/project4/Project4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.project4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
